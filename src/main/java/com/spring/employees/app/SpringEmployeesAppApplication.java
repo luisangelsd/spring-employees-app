43a1aaf6b8b423494b23b4cfd57b9dd8ee98db1c
Luis Angel Sandoval Medina <sandovalguichoo@gmail.com>
2023-01-25 10:25:48 -0600

    inicializando-proyecto-y-coneccion-BD

---
+++ src/main/java/com/spring/employees/app/SpringEmployeesAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.employees.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployeesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/employees/app/SpringEmployeesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.employees.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
