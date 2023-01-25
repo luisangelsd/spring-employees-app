59d60f5bbb2bf07da7555e1a0fa42ab3c650fd29
Luis Angel Sandoval Medina <sandovalguichoo@gmail.com>
2023-01-25 12:09:10 -0600

    metodo-eliminar-job-ok

--- src/main/java/com/spring/employees/app/controllers/ControllerJob.java
+++ src/main/java/com/spring/employees/app/controllers/ControllerJob.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +23,7 @@ import com.spring.employees.app.model.entitys.EntityJob;
 import com.spring.employees.app.model.finalImplementation.IServicesJob;
 
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 
 @CrossOrigin({"*"})
 @RestController
@@ -108,6 +112,38 @@ public class ControllerJob {
 	}
 	
 	
+	//-- Eliminar
+	@DeleteMapping(path = "delete/{id}")
+	public ResponseEntity<?> deleteById(@PathVariable(value = "id") Integer id){
+		
+		try {
+			
+			//-- validar el id
+			if (id<0) {
+				this.dtoResponseJob=new DtoResponseJob(null, false, "El id no puede ser nulo o ser menor a 0");
+				return new ResponseEntity<DtoResponseJob>(this.dtoResponseJob, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+			//-- Validar que exista el job
+			this.entityJob=this.servicesJob.findById(id);
+			if (this.entityJob==null) {
+				this.dtoResponseJob=new DtoResponseJob(null, false, "El Job no existe");
+				return new ResponseEntity<DtoResponseJob>(this.dtoResponseJob, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+			//-- Eliminar
+			this.servicesJob.deleteById(id);
+			
+			//--Regresar respuesta
+			this.dtoResponseJob=new DtoResponseJob(id, true, null);
+			return new ResponseEntity<DtoResponseJob>(this.dtoResponseJob, HttpStatus.OK);
+			
+	
+		} catch (Exception e) {
+			this.dtoResponseJob=new DtoResponseJob(null, false, e.getMessage());
+			return new ResponseEntity<DtoResponseJob>(this.dtoResponseJob, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
 	
 	
